d359f41700cfc2e52faf49826f232e8909cd8dbb
Alfonso  Jiménez Martínez <aljimenez97@gmail.com>
2019-01-25 16:03:14 +0100

    Hidden numbers

---
+++ hidden_numbers/hiddenNumbers.py
@@ -0,0 +1,25 @@
+import sys
+
+inputList = open (sys.argv[1], 'r')
+outputList = open (sys.argv[2], 'w')
+for i, line in enumerate(inputList):
+	myStr = ''
+	if i is 0:
+		cases = line.rstrip()
+	else:
+		length = len(set(line.rstrip()))
+		bigN = 0
+		smallN = 0
+		for j in range(0,length):
+			bigN +=  j*length**(j)
+			if j is (length-1):
+				smallN += length**(length-1)
+			elif j is (length-2):
+				continue
+			else:
+				smallN += (length-j-1)*length**(j)
+		outputList.write('Case #%d: %d\n' % (i, bigN-smallN))
+		
+		
+inputList.close()
+outputList.close()
